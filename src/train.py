57a8e49dd2e3d6c7e2595582ec875b189b29cfed
jake-seunghyukoh <jake@seunghyukoh.com>
2023-08-06 12:54:08 +0900

    Use accelerate

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+from accelerate import Accelerator
+from torch.optim import Adam
 from torch.utils.data import DataLoader
 
 from packages.data_utils import ExampleDataset
@@ -5,13 +7,29 @@ from packages.models import ExampleModel
 
 
 def train():
+    accelerator = Accelerator()
+
     dataset = ExampleDataset()
-    data_loader = DataLoader(dataset, batch_size=32)
+    dataloader = DataLoader(dataset, batch_size=32)
 
     model = ExampleModel()
 
-    for idx, (data, label) in enumerate(data_loader):
+    optimizer = Adam(model.parameters(), lr=1e-3)
+
+    model, dataloader, optimizer = accelerator.prepare(model, dataloader, optimizer)
+
+    for idx, (data, label) in enumerate(dataloader):
         out = model(data)
+
+        loss = out - label  # TODO: Replace with actual loss function
+
+        accelerator.backward(loss)
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+    accelerator.end_training()
+    accelerator.save_state("checkpoints/accelerator_state.pt")
 
 
 if __name__ == "__main__":
