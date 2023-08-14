111fcf7562cbe19751f282f07ced7e1df0269cac
Seunghyuk Oh <jake@seunghyukoh.com>
2023-08-14 12:52:57 +0900

    Fix: Throw error when environment is not set

--- src/packages/utils/wandb.py
+++ src/packages/utils/wandb.py
@@ -8,5 +8,8 @@ def set_wandb():
     WANDB_API_KEY = os.environ.get("WANDB_API_KEY")
     WANDB_PROJECT = os.environ.get("WANDB_PROJECT")
 
+    assert WANDB_API_KEY != "", "Please set WANDB_API_KEY in .env"
+    assert WANDB_PROJECT != "", "Please set WANDB_PROJECT in .env"
+
     os.environ["WANDB_API_KEY"] = WANDB_API_KEY
     os.environ["WANDB_PROJECT"] = WANDB_PROJECT
