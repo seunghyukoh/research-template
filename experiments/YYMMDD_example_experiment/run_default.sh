861dfd76f5090f94bb3811898c5f61f911cd761a
jake-seunghyukoh <jake@seunghyukoh.com>
2023-09-17 01:03:53 +0900

    Add example configs

--- experiments/YYMMDD_example_experiment/run_default.sh
+++ experiments/YYMMDD_example_experiment/run_default.sh
@@ -1,1 +1,3 @@
-python experiments/YYMMDD_example_experiment/experiment.py --config experiments/YYMMDD_example_experiment/configs/default.yaml --run_name example_run
+python experiments/YYMMDD_example_experiment/experiment.py --config experiments/YYMMDD_example_experiment/configs/default.yaml --run_name default
+python experiments/YYMMDD_example_experiment/experiment.py --config experiments/YYMMDD_example_experiment/configs/example1.yaml --run_name example1
+python experiments/YYMMDD_example_experiment/experiment.py --config experiments/YYMMDD_example_experiment/configs/example2.yaml --run_name example2
