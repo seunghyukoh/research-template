1db34064739b350e51c8af770f62edea35a33a91
Seunghyuk Oh <jake@seunghyukoh.com>
2023-09-21 17:56:46 +0900

    Feat: Add gpu utils

--- src/packages/utils/__init__.py
+++ src/packages/utils/__init__.py
@@ -1,5 +1,6 @@
 from .checkpoint import get_last_checkpoint_or_last_model, parse_checkpoint_step
 from .directory import directory_setter
+from .gpu import get_devices
 from .seed import random_seeder
 from .tracker import Tracker
 from .tracker import init as tracker_init
