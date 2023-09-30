a751cbd3554830dd5fed1088ec5bff86455a4ba4
jake-seunghyukoh <jake@seunghyukoh.com>
2023-09-30 12:28:32 +0900

    Add wandb experiment

--- src/packages/experiments/base_experiment.py
+++ src/packages/experiments/base_experiment.py
@@ -8,13 +8,11 @@ class BaseExperiment(ABC):
         name: str,
         run_name: str,
         config,
-        config_dict: Dict,
         device_map: List[int] = None,
     ):
         assert name is not None, "Experiment name cannot be None"
         assert run_name is not None, "Run name cannot be None"
         assert config is not None, "Experiment config cannot be None"
-        assert config_dict is not None, "Experiment config dict cannot be None"
 
         # Experiment name
         self.name = name
@@ -22,26 +20,19 @@ class BaseExperiment(ABC):
         self.run_name = run_name
         # Experiment config
         self.config = config
-        # Experiment config as dict (For the tracker)
-        self.config_dict = config_dict
         # Device map (GPU's to use)
         self.device_map = device_map
-
-        # Add experiment name to config_dict
-        # * Update this dict with any other info you want to track
-        self.config_dict.update(
-            dict(
-                experiment_name=name,
-                device_map=device_map,
-            )
-        )
 
         self.update_args()
 
     @abstractmethod
     def update_args(self):
+        # Ex)
+        # self.model_args = ...
+        # self.data_args = ...
         raise NotImplementedError
 
     @abstractmethod
     def run(self):
+        # This single function should run the entire experiment
         raise NotImplementedError
