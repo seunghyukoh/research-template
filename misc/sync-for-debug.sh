664814419237c26b1ca2a04fa492a2e4ce4ee25e
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-10-24 16:41:09 +0900

    feat: Add remote script execution functionality to sync-for-debug.sh

--- misc/sync-for-debug.sh
+++ misc/sync-for-debug.sh
@@ -12,3 +12,14 @@ function dsync() {
     echo "Syncing debug files to $USER@$HOST:$DST"
     rsync -avzP --progress $SRC -e "ssh -p $PORT" $USER@$HOST:$DST
 }
+
+
+function drun() {
+    # Sync files first
+    dsync
+    
+    # Run script remotely
+    script_args=$1
+    echo "Running debug script on $USER@$HOST"
+    ssh -p $PORT $USER@$HOST "cd $DST && $script_args"
+}
