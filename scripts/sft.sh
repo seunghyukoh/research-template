dcf11b7228b7c0aa914ba67c265be9ae7c25bbff
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-10-24 13:29:46 +0900

    feat: Update sft configurations

--- scripts/sft.sh
+++ scripts/sft.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+
 accelerate launch src/sft.py \
 --config_file experiments/sft/script_args.yaml \
 --config_file experiments/sft/model_config.yaml \
@@ -11,9 +12,9 @@ accelerate launch src/sft.py \
 --output_dir outputs/sft \
 --max_steps 100 \
 --eval_steps 50 \
---per_device_train_batch_size 8 \
---per_device_eval_batch_size 16 \
---gradient_accumulation_steps 8 \
+--per_device_train_batch_size 64 \
+--per_device_eval_batch_size 64 \
+--gradient_accumulation_steps 1 \
 --push_to_hub true \
 --hub_model_id JakeOh/debug \
 --hub_strategy every_save \
