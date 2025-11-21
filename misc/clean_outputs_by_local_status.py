31483a40bcc89d48728642ea994bae6e67c1c267
Seunghyuk Oh <seunghyukoh0@gmail.com>
2025-11-21 15:00:37 +0900

    feat: Add scripts for cleaning unstable output directories

---
+++ misc/clean_outputs_by_local_status.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+from pathlib import Path
+
+import yaml
+
+paths = os.listdir("outputs")
+for path in paths:
+    print(path)
+
+    output_path = Path("outputs") / path
+
+    if output_path.is_dir():
+        status_file = output_path / ".hydra" / "status.yaml"
+        if not status_file.exists():
+            shutil.rmtree(output_path)
+            continue
+
+        with open(status_file, "r") as f:
+            status = yaml.safe_load(f)
+            if status["status"] != "success":
+                shutil.rmtree(status_file.parent)
---
+++ misc/clean_outputs_by_wandb.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from wandb import Api
+
+load_dotenv()
+
+WANDB_ENTITY = os.getenv("WANDB_ENTITY")
+WANDB_PROJECT = os.getenv("WANDB_PROJECT")
+
+api = Api()
+
+stable_runs = api.runs(f"{WANDB_ENTITY}/{WANDB_PROJECT}", filters={"group": "stable"})
+stable_run_ids = set([run.id for run in stable_runs])
+
+paths = os.listdir("outputs")
+unstable_run_ids = set(paths) - stable_run_ids
+
+if len(unstable_run_ids) == 0:
+    print("No unstable runs found")
+    exit()
+
+print(f"We found {len(unstable_run_ids)} unstable runs")
+print("First 5 directories to be removed:")
+for i, path in enumerate(list(unstable_run_ids)[:5]):
+    print(f"  {path}")
+
+confirmation = input("\nDo you want to proceed with removing these directories? (y/N): ")
+if confirmation.lower() != "y":
+    print("Aborted.")
+    exit()
+
+print("Removing unstable runs...")
+
+for path in list(unstable_run_ids):
+    print(f"Removing output: {path}")
+    shutil.rmtree(Path("outputs") / path)
